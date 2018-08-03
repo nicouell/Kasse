4f967e21781d40cb4ed9a545f4e8b75f2142412a
Nicola Ouellet <nicola.ouellet@gmail.com>
2018-08-03 11:51:22 -0400

    setup server connection and router

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://kasse1:kasse1@ds113122.mlab.com:13122/kassedev"
+};
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const wallets = require("./routes/api/wallets");
 
 const app = express();
 
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use("/api/users", users);
+app.use("/api/wallets", wallets);
 
 const port = process.env.PORT || 5000;
 
